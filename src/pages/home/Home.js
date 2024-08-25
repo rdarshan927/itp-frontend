5a48258ead0f12e7c0b561b18952842a246611e1
rdarshan927 <rdarshan927@gmail.com>
2024-08-25 16:13:13 +0530

    moved sample usage of logout from Home.js to Sample.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,13 +3,15 @@ import React from "react";
 //import home components
 import Hero from "./Hero";
 import Flower from "./Flower";
+import Sample from "./Sample";
 
 const Home =()=> {
     
     return (
         <div>
-           <Hero />
-           <Flower />
+            <Hero />
+            <Flower />
+            <Sample />
         </div>
     )
 }
