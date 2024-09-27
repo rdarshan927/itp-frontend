df2e182fb009ef90a9e8872998c47bc4cb53bb66
Chanupa deshan <chanupadeshan2002@gmail.com>
2024-09-27 13:33:26 +0530

    to pull

--- src/pages/admin/components/AdminHeader.js
+++ src/pages/admin/components/AdminHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 const AdminHeader = ({ username }) => {
     return (
         <div className="bg-darkG">
-            <div className="flex justify-end items-center p-6">
+            <div className="flex justify-end items-center p-2">
                 <h1 className="text-white text-3xl font-bold">Financial Manager</h1>
                 <img src="/image/boy.png" alt="User Avatar" className="w-16 h-16 mr-4 ml-4" />
 
--- src/pages/admin/components/SideBar.js
+++ src/pages/admin/components/SideBar.js
@@ -4,19 +4,15 @@ const Sidebar = () => {
 
 
     return (
+        
         <div className="bg-lightG p-3 ">
-            <div className="inline-grid relative top-48">
-            <button className="text-2xl bg-darkG text-white font-bold py-2 text rounded-b-lg mt-10 w-72 rounded hover:bg-[#c9d5b0]">Dashboard</button>
-
+            <div className="inline-grid relative top-20">
+                 <button className="text-2xl bg-darkG text-white font-bold py-2 text rounded-b-lg mt-10 w-72 rounded hover:bg-[#c9d5b0]">Dashboard</button>
                 <button className="text-2xl bg-darkG text-white font-bold py-2 text rounded-b-lg mt-10 w-72 rounded hover:bg-[#c9d5b0]">Salary</button>
                 <button className="text-2xl bg-darkG text-white font-bold py-2 text rounded-b-lg mt-10 w-72 rounded hover:bg-[#c9d5b0]">Inventory Stuff</button>
+                <button className="text-2xl bg-darkG text-white font-bold py-2 text rounded-b-lg mt-10 w-72 rounded hover:bg-[#c9d5b0]">Logout</button>
 
             
-            </div>
-
-            <div className="relative top-96">
-            <button className="text-2xl bg-darkG text-white font-bold py-2 text rounded-b-lg mt-10 w-72 rounded hover:bg-[#c9d5b0]">Logout</button>
-
             </div>
         </div>
     )
