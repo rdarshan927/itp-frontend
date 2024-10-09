b46a9f63c9e5ed613de46ac1f64a38794f9370be
Nuveen <nuveen.rakeesha@icloud.com>
2024-10-09 16:45:49 +0530

    plant scheduling completed

---
+++ src/pages/admin/PlantSchedule/PlantSchedule.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AddPlantSchedule from "./AddSchedule";
+import ViewPlantSchedule from "./ViewSchedule";
+
+const PlantSchedule = () => {
+    return (
+        <div>
+            <AddPlantSchedule />
+            <ViewPlantSchedule />
+        </div>
+    );
+}
+
+export default PlantSchedule;
