3f48a91b48ec25aa25a82312592ae589b8d571e8
Darshan R <rdarshan927@gmail.com>
2024-10-12 02:44:10 +0530

    Update Allroutes.js
    don't edit unnecessarily!

--- src/Allroutes.js
+++ src/Allroutes.js
@@ -6,60 +6,39 @@ import "react-toastify/dist/ReactToastify.css";
 import Loading from "./components/Pageloading";
 
 // Lazy loading the components
-const Home = lazy(() => import("./pages/home/Home"));
-const ManageEmployeeRole = lazy(() =>
-  import("./pages/admin/ManageEmployeeRole/ManageEmployeeRole")
-);
-const Invoice = lazy(() => import("./pages/admin/sales/Invoice"));
-const SalesDashboard = lazy(() => import("./pages/admin/sales/SalesDashboard"));
-const Cart = lazy(() => import("./pages/cart/Cart"));
-const PaymentSuccess = lazy(() => import("./pages/cart/PaymentSuccess"));
-const Register = lazy(() => import("./pages/register/Register"));
-const ForgotPassword = lazy(() =>
-  import("./pages/forgetpassword/ForgetPassword")
-);
-const ResetPassword = lazy(() =>
-  import("./pages/forgetpassword/ResetPassword")
-);
-const AddEmployee = lazy(() =>
-  import("./pages/admin/SheporaEmployeeManagement/AddEmployee")
-);
-
-const Login = lazy(() => import("./pages/login/Login"));
-const HarvestAdd = lazy(() =>
-  import("./pages/admin/harvest_manager/HarvestAdd")
-);
-const Table = lazy(() => import("./pages/admin/harvest_manager/Harves_table"));
-const ResourceInventory = lazy(() =>
-  import("./pages/admin/ManageInventory/ResourceInventory")
-);
-const SalesInventory = lazy(() =>
-  import("./pages/admin/ManageInventory/SalesInventory")
-);
-const Packing = lazy(() => import("./pages/admin/packinganddelivery/Packing"));
-const Order = lazy(() => import("./pages/admin/packinganddelivery/Order"));
-const Delivery = lazy(() =>
-  import("./pages/admin/packinganddelivery/Delivery")
-);
-const Salary = lazy(() => import("./pages/admin/Salary/Salary"));
-const InventoryStuff = lazy(() =>
-  import("./pages/admin/InventoryStuff/InventoryStuff")
-);
-const Dashboard = lazy(() => import("./pages/admin/Dashboard/Dashboard"));
-const AdminLogin = lazy(() => import("./pages/login/AdminLogin"));
-const Plant = lazy(() => import("./pages/admin/PlantSchedule/PlantSchedule"));
-const PlantReports = lazy(() =>
-  import("./pages/admin/PlantSchedule/PlantReport")
-);
+const Home = lazy(() => import('./pages/home/Home'));
+const ManageEmployeeRole = lazy(() => import('./pages/admin/ManageEmployeeRole/ManageEmployeeRole'));
+const Invoice = lazy(() => import('./pages/admin/sales/Invoice'))
+const SalesDashboard = lazy(() => import('./pages/admin/sales/SalesDashboard'));
+const Cart = lazy(() => import('./pages/cart/Cart'))
+const PaymentSuccess = lazy(() => import('./pages/cart/PaymentSuccess'))
+const Register = lazy(() => import('./pages/register/Register'))
+const ForgotPassword = lazy(() => import('./pages/forgetpassword/ForgetPassword'));
+const ResetPassword = lazy(() => import('./pages/forgetpassword/ResetPassword'));
+const AddEmployee = lazy(() => import('./pages/admin/SheporaEmployeeManagement/AddEmployee'));
+const Login = lazy(() => import('./pages/login/Login'));
+const HarvestAdd = lazy(() => import('./pages/admin/harvest_manager/HarvestAdd'));
+const Table = lazy(() => import('./pages/admin/harvest_manager/Harves_table'));
+const ResourceInventory = lazy(() => import('./pages/admin/ManageInventory/ResourceInventory'));
+const SalesInventory = lazy(() => import('./pages/admin/ManageInventory/SalesInventory'));
+const Packing = lazy(() => import('./pages/admin/packinganddelivery/Packing'));
+const Order = lazy(() => import('./pages/admin/packinganddelivery/Order'));
+const Delivery = lazy(() => import('./pages/admin/packinganddelivery/Delivery'));
+const Salary = lazy(() => import('./pages/admin/Salary/Salary'));
+const InventoryStuff = lazy(() => import('./pages/admin/InventoryStuff/InventoryStuff'));
+const Dashboard = lazy(() => import('./pages/admin/Dashboard/Dashboard'));
+const AdminLogin =lazy(()=>import('./pages/login/AdminLogin'))
+const Plant = lazy(() => import('./pages/admin/PlantSchedule/PlantSchedule'));
+const PlantReports = lazy(() => import('./pages/admin/PlantSchedule/PlantReport'));
+const Flower = lazy(() => import("./pages/home/Flower"));
+const Bouquet = lazy(() => import("./pages/home/Bouquet"));
+const ItemDescription = lazy(() => import("./pages/home/ItemDescription"));
 
 const ClientLayout = lazy(() => import("./layouts/ClientLayout"));
 const AdminLayout = lazy(() => import("./layouts/AdminLayout"));
 const ClientPrivateRoute = lazy(() => import("./config/ClientPrivateRoute"));
 const AdminPrivateRoute = lazy(() => import("./config/AdminPrivateRoute"));
 const None = lazy(() => import("./pages/admin/components/None"));
-const Flower = lazy(() => import("./pages/home/Flower"));
-const Bouquet = lazy(() => import("./pages/home/Bouquet"));
-const ItemDescription = lazy(() => import("./pages/home/ItemDescription"));
 
 const AllRoutes = () => {
   return (
@@ -87,10 +66,7 @@ const AllRoutes = () => {
               />
               <Route path="/register" element={<Register />} />
               <Route path="/forgetpassword" element={<ForgotPassword />} />
-              <Route
-                path="/reset_password/:id/:token"
-                element={<ResetPassword />}
-              />
+              <Route path="/reset_password/:id/:token" element={<ResetPassword />} />
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/adminLogin" element={<AdminLogin />} />
@@ -222,20 +198,17 @@ const AllRoutes = () => {
                   </AdminPrivateRoute>
                 }
               />
-              <Route path="/manageemprole" element={<ManageEmployeeRole />} />
-              <Route path="/harvest" element={<HarvestAdd />} />
-              <Route path="/table" element={<Table />} />
-              <Route path="/manageemprole" element={<ManageEmployeeRole />} />
-              <Route path="/addemployee" element={<AddEmployee />} />
-              <Route path="/packing" element={<Packing />} />
-              <Route path="/order" element={<Order />} />
-              <Route path="/delivery" element={<Delivery />} />
+              <Route path='/manageemprole' element={<ManageEmployeeRole />} />   
+              <Route path='/harvest' element={<HarvestAdd />} />
+              <Route path='/table' element={<Table />} />
+              <Route path='/manageemprole' element={<ManageEmployeeRole />} />
+              <Route path='/addemployee' element={<AddEmployee />} />
+              <Route path='/packing' element={<Packing />} />
+              <Route path='/order' element={<Order />} />
+              <Route path='/delivery' element={<Delivery />} />
 
               <Route path="/sales/invoices" element={<Invoice />} />
-              <Route
-                path="/ResourceInventory"
-                element={<ResourceInventory />}
-              />
+              <Rout path="/ResourceInventory" element={<ResourceInventory />}/>
               <Route path="/SalesInventory" element={<SalesInventory />} />
               <Route path="sales/dashboard" element={<SalesDashboard />} />
               <Route path="/salary" element={<Salary />} />
